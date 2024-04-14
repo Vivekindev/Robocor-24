f2df91af78b4789eadaeaa8ebc75bfaaeb61cc9b
vivekindev <vivekindev@gmail.com>
2024-04-14 23:28:43 +0530

    commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "./client/dist")));
+app.use(express.static(path.join(__dirname, "./")));
+
+app.get('*', function(_, res) {
+    res.sendFile(path.join(__dirname, "./client/dist/index.html"), function(err) {
+        res.status(500).send(err);
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening at port ${PORT}`);
+});
