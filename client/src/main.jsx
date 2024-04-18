9b617395d40b199379d61c8ba5b3d68dc88492d1
vivekindev <vivekindev@gmail.com>
2024-04-18 19:53:22 +0530

    iframe-payment-gateway

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Menu from './menu.jsx'
-import Btn from './btn.jsx'
-import Home from './home.jsx'
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home />
-    
-    
+    <App />
   </React.StrictMode>,
 )
---
+++ client/src/register.jsx
@@ -0,0 +1,16 @@
+const Register = ()=>{
+
+return(
+<>
+<div style={{
+width:"100%",
+height:"100vh",
+backgroundColor:"#110302"
+}}>
+<iframe width="100%" height="100%" style={{border:"none"}} src="https://www.yepdesk.com/embed/buy-tickets/6620d34946e0fb0001adb86f/private/a42cdaahsl"></iframe>
+</div>
+</>
+);
+
+}
+export default Register;
