be16e2153e0002c0a9a1935110c1ed648e93ab11
vivekindev <vivekindev@gmail.com>
2024-04-21 18:49:51 +0530

    bugFixDownloadHandler

--- client/src/functions/handleDownload.jsx
+++ client/src/functions/handleDownload.jsx
@@ -1,5 +1,5 @@
 const handleDownload = (fileName) => {
-    const pdfUrl =  `http://localhost:5173/src/assets/${fileName}`;
+    const pdfUrl =  `/assets/${fileName}`;
     const link = document.createElement('a');
     link.href = pdfUrl;
     link.download = 'Robocor24_RuleBook.pdf';
