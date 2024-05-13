425c99b5c1e8174ff5b2e2cf96bbf0d8e501e604
vivekindev <vivekindev@gmail.com>
2024-05-13 21:39:55 +0530

    index.js cache clearing

--- index.js
+++ index.js
@@ -1,44 +1,49 @@
-import express from 'express'
-import path from 'path'
+import express from 'express';
+import path from 'path';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import handleVerification from './functions/handleVerification.js'
+import handleVerification from './functions/handleVerification.js';
+
 const __dirname = path.resolve();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true })); // Add this line
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, "./client/dist")));
-app.use(express.static(path.join(__dirname, "./")));
-
-app.get('*/abstractProjectSymposium', function(_, res) {
-    res.sendFile(path.join(__dirname, "./client/dist/assets/ppt_template.pptx"), function(err) {
-        res.status(500).send(err);
-    });
+// Middleware to set cache control headers for static assets
+app.use((req, res, next) => {
+  res.setHeader('Cache-Control', 'no-store, must-revalidate');
+  next();
 });
 
-app.get('*', function(_, res) {
-    res.sendFile(path.join(__dirname, "./client/dist/index.html"), function(err) {
-        res.status(500).send(err);
-    });
+app.use(express.static(path.join(__dirname, './client/dist')));
+app.use(express.static(path.join(__dirname, './')));
+
+app.get('*/abstractProjectSymposium', (_, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/assets/ppt_template.pptx'), function(err) {
+    res.status(500).send(err);
+  });
 });
 
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, './client/dist/index.html'), function(err) {
+    res.status(500).send(err);
+  });
+});
 
-app.post('/api/verifyCert',  async(req, res) => {
-  try{
-     const name = await handleVerification(req.body.email, req.body.event)
-     res.send({name:name});
+app.post('/api/verifyCert', async (req, res) => {
+  try {
+    const name = await handleVerification(req.body.email, req.body.event);
+    res.send({ name: name });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send(err.message);
   }
-  catch(err){
-    console.error(err)
-  }
 });
-
 
 app.listen(PORT, () => {
-    console.log(`Listening at port ${PORT}`);
+  console.log(`Listening at port ${PORT}`);
 });
